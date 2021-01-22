9094ad69f9de53d6c20434d8e70c04e215176a0f
jidegithub <olutolajide@gmail.com>
2021-01-23 00:38:25 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header text-lg">
+        hi
+      </header>
+    </div>
+  );
+}
+
+export default App;

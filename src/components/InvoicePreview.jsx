97faa859e971501efa8b73544b5366f58107eb15
jidegithub <olutolajide@gmail.com>
2021-01-23 21:02:21 +0100

    update: populate invoice receipt

--- src/components/InvoicePreview.jsx
+++ src/components/InvoicePreview.jsx
@@ -77,25 +77,25 @@ export default function InvoicePreview() {
           <div className="border border-t-0 border-gray-200 mb-8 px-1"></div>
 
 
-          <div class="py-2 ml-auto mt-5 w-full sm:w-2/4 lg:w-1/4">
+          <div class="py-2 ml-auto mt-5 w-full flex items-end">
             <div class="flex justify-between mb-3">
-              <div class="text-gray-800 text-right flex-1">Total incl. GST</div>
+              <div class="text-gray-800 text-right flex-1">SUB-TOTAL</div>
               <div class="text-right w-40">
-                <div class="text-gray-800 font-medium" x-html="netTotal"></div>
+                <div class="text-gray-800 font-medium" x-html="netTotal">NGN 45, 000</div>
               </div>
             </div>
             <div class="flex justify-between mb-4">
-              <div class="text-sm text-gray-600 text-right flex-1">GST(18%) incl. in Total</div>
+              <div class="text-sm text-gray-600 text-right flex-1">VAT(7.5%)</div>
               <div class="text-right w-40">
-                <div class="text-sm text-gray-600" x-html="totalGST"></div>
+                <div class="text-sm text-gray-600" x-html="totalGST">NGN 3,375</div>
               </div>
             </div>
           
             <div class="py-2 border-t border-b">
               <div class="flex justify-between">
-                <div class="text-xl text-gray-600 text-right flex-1">Amount due</div>
+                <div class="text-xl text-gray-600 text-right flex-1">TOTAL</div>
                 <div class="text-right w-40">
-                  <div class="text-xl text-gray-800 font-bold" x-html="netTotal"></div>
+                  <div class="text-xl text-gray-800 font-bold" x-html="netTotal">NGN 48, 375</div>
                 </div>
               </div>
             </div>

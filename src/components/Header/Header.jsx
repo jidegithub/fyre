caed46cdd80266c011e3ff63664931f5988ce028
jidegithub <olutolajide@gmail.com>
2021-01-25 19:48:57 +0100

    update: update navbar mobile view II

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -40,18 +40,18 @@ export default function NavBar() {
               <UserIcon />
               <CaretDownIcon />
             </div>
-            <button className="hide flex items-center border border-blue ml-2 px-3 py-2 rounded text-blue"
-            onClick={() => toggleExpansion(!isExpanded)}
-          >
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+            <button className="menu flex items-center border border-blue ml-2 px-3 py-2 rounded text-blue"
+              onClick={() => toggleExpansion(!isExpanded)}
             >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
+              <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
           </div>
 
         </div>
